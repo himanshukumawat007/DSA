3f61a3acc47db7e672d2fd91451638c9d39160ca
himanshu <kumawathimanshu200@gmail.com>
2026-07-01 19:28:05 +0530

    added Cunting words and vowels in string

--- src/main/java/Strings/CountingWordsAndVowels.java
+++ src/main/java/Strings/CountingWordsAndVowels.java
@@ -1,9 +1,23 @@
 package Strings;
 
+
 public class CountingWordsAndVowels {
     public static void main(String[] args){
-        String str ="hello";
+        String str ="How are you";
         char[] arr= str.toCharArray();
-        System.out.print(arr.length);
+        int vcount=0;
+        int words=1;
+        for (char c : arr) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+                vcount++;
+            }
+            if (c == ' ') {
+                words++;
+            }
+
+        }
+      System.out.println("Sentence: " + new String(arr));
+        System.out.println("no. of vowels : "+ vcount);
+        System.out.print("no. of words :" + words );
     }
 }
